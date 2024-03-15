c8a64c607706fa40b4f8e15aadcb2f56a978c6d6
Trần Hoài Bảo <52000634@student.tdtu.edu.vn>
2024-03-15 21:21:51 +0700

    Change SQL up v2 book-management-v2

--- pkg/database/migration/002_book-management-v2.up.sql
+++ pkg/database/migration/002_book-management-v2.up.sql
@@ -9,13 +9,6 @@ CREATE TABLE "author" (
 INSERT INTO author("name", birth_day)
 SELECT "author", null from book;
 
-INSERT INTO author("name", birth_day) 
-VALUES 
-    ('Amit Garg', '1978-03-18'),
-    ('Lalit Kumar', '1970-01-01'),
-    ('Sharad Kumar Verma', '1987-06-24'),
-    ('James Clear', '1986-01-22');
-
 ALTER TABLE book
     ADD COLUMN author_id INT,
     ADD CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES author(id);
